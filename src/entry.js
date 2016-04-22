b2af0069f2eae67a79d97f5639c0bcfc832b04b6
Ben Wilcox <reccanti@gmail.com>
2016-04-21 22:38:10 -0400

    Added 'Plotter' as Export
    the 'createPlotter' function is now exported explicitly as
    module.exports.Plotter. It was perviously overwriting the
    module.exports object

--- src/entry.js
+++ src/entry.js
@@ -1,4 +1,4 @@
 module.exports.Point = require('./point');
 module.exports.Line = require('./line');
 module.exports.Plot = require('./plot');
-module.exports = require('./plotter');
+module.exports.Plotter = require('./plotter');
