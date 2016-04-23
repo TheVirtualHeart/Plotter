94099b201fd4d554c49a16add3464c46ac493d3e
Ben Wilcox <reccanti@gmail.com>
2016-04-23 01:41:42 -0400

    Added Plot Tests
    Added more tests for Plot, mostly centering around the settings

--- tests/js/line.js
+++ tests/js/line.js
@@ -10,11 +10,11 @@ describe('A suite of tests against the Line object', function () {
             var p2 = new Point(3, 4);
             var l = new Line(p1, p2);
             
-            expect(l.a).to.be.an('object');
+            expect(l.a).to.be.a.Point;
             expect(l.a.x).to.eql(1);
             expect(l.a.y).to.eql(2);
             
-            expect(l.b).to.be.an('object');
+            expect(l.b).to.be.a.Point;
             expect(l.b.x).to.eql(3);
             expect(l.b.y).to.eql(4);
             
@@ -36,7 +36,7 @@ describe('A suite of tests against the Line object', function () {
         
         it('allows the User to enter point-and-slope arguments in a two-point initialization', function (done) {
             var l = new Line(new Point(3, 4), 5);
-            expect(l.a).to.be.an('object');
+            expect(l.a).to.be.a.Point;
             expect(l.a.x).to.eql(3)
             expect(l.a.y).to.eql(4);;
             expect(l.b).to.eql(5);
@@ -44,21 +44,6 @@ describe('A suite of tests against the Line object', function () {
             expect(l.sloe).to.be.undefined;
             done();
         }); 
-        
-        // it('tests a Point initialized with one parameters', function (done) {
-        //     var p = new Point(3);
-        //     expect(p.x).to.eql(3);
-        //     expect(p.y).to.be.undefined;
-        //     done();
-        // }); 
-        
-        // it('tests a Point initialized with letters for parameters', function (done) {
-        //     var p = new Point('one', 'b');
-        //     expect(p.x).to.eql('one');
-        //     expect(p.y).to.eql('b');
-        //     done();
-        // }); 
-        
     });
     
 });
